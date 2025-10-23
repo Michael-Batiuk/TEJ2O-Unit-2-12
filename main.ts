ce37e7f1b815189fe1a75ed39da72dd90488a542
Michael-Batiuk <Michael.Batiuk@stu.ocsb.ca>
2025-10-23 13:26:42 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,47 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Michael Batiuk
+ * Created on: Oct 2025
+ * This program checks distance.
 */
 
-basic.showString('Hello, World!')
+//variables
+let sonarDistance: number = 0
+let neopixelStrip: neopixel.Strip = null
+
+//setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+//a button
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    sonarDistance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    if (sonarDistance < 10) {
+        neopixelStrip = neopixel.create(DigitalPin.P16, 1, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+        basic.pause(3000)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.clearScreen()
+        basic.showIcon(IconNames.Happy)
+    } else {
+        // Turn neopixel green
+        neopixelStrip = neopixel.create(DigitalPin.P16, 1, NeoPixelMode.RGB)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.show()
+        basic.pause(3000)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.show()
+        basic.clearScreen()
+        basic.showIcon(IconNames.Happy)
+    }
+
+})
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
